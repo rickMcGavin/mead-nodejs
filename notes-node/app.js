599e44c54efd9d54f0432c0a6e691f0256f40743
rebelBeard <rfm144@gmail.com>
2017-09-26 22:57:23 -0400

    Completed videso 11-13.5

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,18 +1,15 @@
 console.log('starting app');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-//var res = notes.addNote();
-//console.log(res);
+var command = process.argv[2];
+console.log('Command: ', command);
 
-console.log(notes.addNum(1,2));
-
-//var user = os.userInfo();
-
-//fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} `, function(err) {
-//	if (err) {
-//		console.log('Unable to write to file');
-//	}
-//});
+if (command === 'add') {
+	console.log('Adding new note');
+} else if (command === 'list') {
+	console.log('Listing all notes');
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -4,7 +4,3 @@ module.exports.addNote = () => {
 	console.log('addNote');
 	return 'New note';
 }
-
-module.exports.addNum = (a, b) => {
-	return a + b;
-}
