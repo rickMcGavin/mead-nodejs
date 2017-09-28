f62de3e032c5a7d688e95eb7a4eb9a6efb131ca3
Rick <rmcgavin@redventures.com>
2017-09-27 21:21:59 -0400

    updating progress

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,14 +2,23 @@ console.log('starting app');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command: ', command);
+console.log('Yargs', argv);
 
 if (command === 'add') {
-	console.log('Adding new note');
+	notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-	console.log('Listing all notes');
+	notes.getAll();
+} else if (command === 'read') {
+	console.log('Reading note');
+} else if (command === 'remove') {
+	console.log('Removing note');
+} else {
+	console.log('Command not recognized');
 }
